d8f2171afa1f638c81845117c42eed868ba92a71
vacuuum <47915224+vacuuum@users.noreply.github.com>
2024-08-29 23:28:13 +0530

    Simplied conditional logic to reduce CCN

--- checker.c
+++ checker.c
@@ -16,24 +16,19 @@ BatterySts_en batteryIsOk(float temperature, float soc, float chargeRate) {
 
   BatterySts_en ret = BATTERY_OK;
 
-  if ((temperature < 0 || temperature > 45) || 
-  (soc < 20 || soc > 80) || 
-  (chargeRate > 0.8)) 
+  if (temperature < 0 || temperature > 45) 
   {
-    if (temperature < 0 || temperature > 45)
-    {
-      ret = TEMP_OUT_OF_RANGE;
-    }
-    else if (soc < 20 || soc > 80)
-    {
-      ret = SOC_OUT_OF_RANGE;
-    }
-    else
-    {
-    ret = CHARGE_RATE_OUT_OF_RANGE;
-    }
+    return TEMP_OUT_OF_RANGE;
   }
-  return ret;
+  if (soc < 20 || soc > 80) 
+  {
+    return SOC_OUT_OF_RANGE;
+  }
+  if (chargeRate > 0.8) 
+  {
+    return CHARGE_RATE_OUT_OF_RANGE;
+  }
+  return BATTERY_OK;
 }
 
 int main() 
