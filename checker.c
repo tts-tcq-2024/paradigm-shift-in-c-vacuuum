e4e5502a831cb36bdd9a58ff452dd175a42b80f0
vacuuum <47915224+vacuuum@users.noreply.github.com>
2024-08-30 12:37:53 +0530

    Added more functions to reduce CCN of batteryIsOk function

--- checker.c
+++ checker.c
@@ -5,12 +5,14 @@
 // enumeration to avoid multiple print statements
 typedef enum 
 {
+  BATTERY_OK,
   TEMP_OUT_OF_RANGE,
   SOC_OUT_OF_RANGE,
   CHARGE_RATE_OUT_OF_RANGE
 } BatterySts_en;
 
 // Separate functions for each condition
+
 int isTempOrSocOutOfRange(float qty, float ll, float ul) 
 {
   return (qty < ll || qty > ul);
@@ -21,27 +23,57 @@ int isChargeRateOutOfRange(float chargeRate)
   return (chargeRate > 0.8);
 }
 
-
-BatterySts_en batteryIsOk(float temperature, float soc, float chargeRate)
+BatterySts_en checkTemperature(float temperature)
 {
-  if (isTempOrSocOutOfRange(temperature,0,45))
+  if (isTempOrSocOutOfRange(temperature, 0, 45))
   {
     return TEMP_OUT_OF_RANGE;
   }
-  if (isTempOrSocOutOfRange(soc,20,80))
+  return BATTERY_OK;
+}
+
+BatterySts_en checkSoc(float soc)
+{
+  if (isTempOrSocOutOfRange(soc, 20, 80))
   {
     return SOC_OUT_OF_RANGE;
   }
+  return BATTERY_OK;
+}
+
+BatterySts_en checkChargeRate(float chargeRate)
+{
   if (isChargeRateOutOfRange(chargeRate))
   {
     return CHARGE_RATE_OUT_OF_RANGE;
   }
+  return BATTERY_OK;
+}
+
+BatterySts_en batteryIsOk(float temperature, float soc, float chargeRate)
+{
+  BatterySts_en status;
+
+  status = checkTemperature(temperature);
+  if (status != BATTERY_OK) 
+  {
+    return status;
+  }
+
+  status = checkSoc(soc);
+  if (status != BATTERY_OK) 
+  {
+    return status;
+  }
+
+  return checkChargeRate(chargeRate);
 }
 
 int main() 
 {
-  assert(batteryIsOk(-30, 70, 0.7) == 0);
-  assert(batteryIsOk(40, 10, 0) == 1);
-  assert(batteryIsOk(25, 70, 0.9) == 2);
-  assert(!batteryIsOk(20, 50, 0.5));
+  assert(batteryIsOk(-30, 70, 0.7) == 1);
+  assert(batteryIsOk(40, 10, 0) == 2);
+  assert(batteryIsOk(25, 70, 0.9) == 3);
+  assert(batteryIsOk(20, 50, 0.5) == 0);
 }
+
